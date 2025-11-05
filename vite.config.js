ade2b7787b47e6870f7ef15f0b4c5b699aac1916
argyris3 <bigol3@hotmail.com>
2025-11-05 11:55:39 +0200

    cors commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    cors: false
+  },
   plugins: [react(), tailwindcss()],
   base: '/',
 
